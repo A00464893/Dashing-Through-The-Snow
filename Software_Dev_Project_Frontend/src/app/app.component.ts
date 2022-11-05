25ef6f01f6d031d69f0c68e2af0a5f6cac194f7d
A00464893 <piyush.priyam@smu.ca>
2022-11-05 09:20:13 +0530

    Ui loader added
    Signed-off-by: A00464893 <piyush.priyam@smu.ca>

--- Software_Dev_Project_Frontend/src/app/app.component.ts
+++ Software_Dev_Project_Frontend/src/app/app.component.ts
@@ -6,15 +6,46 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   public forecasts?: WeatherForecast[];
+  config = {
+    "bgsColor": "red",
+    "bgsOpacity": 0.5,
+    "bgsPosition": "bottom-center",
+    "bgsSize": 40,
+    "bgsType": "rectangle-bounce",
+    "blur": 5,
+    "delay": 0,
+    "fastFadeOut": false,
+    "fgsColor": "#00ACC1",
+    "fgsPosition": "center-center",
+    "fgsSize": 60,
+    "fgsType": "chasing-dots",
+    "gap": 24,
+    "logoPosition": "center-center",
+    "logoSize": 120,
+    "logoUrl": "",
+    "masterLoaderId": "master",
+    "overlayBorderRadius": "0",
+    "overlayColor": "rgba(40, 40, 40, 0.8)",
+    "pbColor": "#00ACC1",
+    "pbDirection": "ltr",
+    "pbThickness": 5,
+    "hasProgressBar": true,
+    "text": "",
+    "textColor": "#FFFFFF",
+    "textPosition": "center-center",
+    "maxTime": -1,
+    "minTime": 300
+}
 
   constructor(http: HttpClient) {
     http.get<WeatherForecast[]>('/weatherforecast').subscribe(result => {
       this.forecasts = result;
     }, error => console.error(error));
   }
-
+  
   title = 'Software_Dev_Project';
 }
 
--- Software_Dev_Project_Frontend/src/app/app.module.ts
+++ Software_Dev_Project_Frontend/src/app/app.module.ts
@@ -1,18 +1,101 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+import { SocialLoginModule, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
+import { GoogleLoginProvider, FacebookLoginProvider } from '@abacritt/angularx-social-login';
+import {
+  NgxUiLoaderModule,
+  NgxUiLoaderConfig,
+  SPINNER,
+  POSITION,
+  PB_DIRECTION,
+  NgxUiLoaderRouterModule,
+  NgxUiLoaderHttpModule
+} from 'ngx-ui-loader';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
 
+
+const ngxUiLoaderConfig: NgxUiLoaderConfig = {
+      
+    "bgsColor": "red",
+    "bgsOpacity": 0.5,
+    "bgsPosition": "bottom-center",
+    "bgsSize": 40,
+    "bgsType": "rectangle-bounce",
+    "blur": 5,
+    "delay": 0,
+    "fastFadeOut": false,
+    "fgsColor": "#00ACC1",
+    "fgsPosition": "center-center",
+    "fgsSize": 60,
+    "fgsType": "chasing-dots",
+    "gap": 24,
+    "logoPosition": "center-center",
+    "logoSize": 120,
+    "logoUrl": "",
+    "masterLoaderId": "master",
+    "overlayBorderRadius": "0",
+    "overlayColor": "rgba(40, 40, 40, 0.8)",
+    "pbColor": "#00ACC1",
+    "pbDirection": "ltr",
+    "pbThickness": 5,
+    "hasProgressBar": true,
+    "text": "",
+    "textColor": "#FFFFFF",
+    "textPosition": "center-center",
+    "maxTime": -1,
+    "minTime": 300
+  
+};
+
+const routes: Routes = []
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, BrowserAnimationsModule
+    BrowserModule, HttpClientModule, RouterModule.forRoot(routes), BrowserAnimationsModule, SocialLoginModule, MatAutocompleteModule, MatBadgeModule, MatBottomSheetModule,
+    MatButtonModule, MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDialogModule,
+    MatDividerModule, NgxUiLoaderModule.forRoot(ngxUiLoaderConfig), NgxUiLoaderRouterModule, NgxUiLoaderHttpModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              'clientId'
+            )
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('clientId')
+          }
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    }
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
