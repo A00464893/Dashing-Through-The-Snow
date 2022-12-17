437b817f22e7d92df25435f9ecb2a46cffb7bdf8
A00466455 <Shiney.Prabhakar@smu.ca>
2022-12-17 03:10:13 -0400

    Created CartItem and GeoDetails models and controller

---
+++ Software_Dev_Project_Backend/Models/CartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public String? Username { get; set; }
+        public string? Session_id { get; set; }
+        public string? product_id { get; set; }
+      
+    }
+}
