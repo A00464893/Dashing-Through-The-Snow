9dda64bbb79bb1fe59ee043beab20411463a31d1
A00466455 <Shiney.Prabhakar@smu.ca>
2022-12-18 12:44:44 -0400

    adding product.cs

---
+++ Software_Dev_Project_Backend/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Backend.Models
+{
+    public class Product
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Desc { get; set; }
+        public string? image { get; set; }
+        public string? category_id { get; set; }
+        public int quantity { get; set; }
+        public int price { get; set; }
+
+    }
+}
